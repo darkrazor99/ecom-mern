6bd950f594f7080c75d4ea9cd924fbad3563c8aa
tarek <36449641+darkrazor99@users.noreply.github.com>
2023-08-02 07:04:41 +0300

    Update dbConnect.js

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose");
 
 const dbConnect = () => {
     try {
-        const conn = mongoose.connect("mongodb+srv://firstUser:Lqsda5wFggCZbjPt@cluster0.cyl8rxt.mongodb.net/mern1");
+        const conn = mongoose.connect("mongodb+srv://firstUser:@cluster0.cyl8rxt.mongodb.net/mern1");
         console.log("database connected succesfuly");
     } catch (error) {
         console.log("Database error");
